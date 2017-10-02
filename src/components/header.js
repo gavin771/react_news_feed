cbb59bfb4ff4064d8d68ef834b13be62bd388731
gavin771 <gavin.samuels10@gmail.com>
2017-10-01 21:47:06 -0500

    added basic state update feature

--- src/components/header.js
+++ src/components/header.js
@@ -2,17 +2,26 @@ import React, { Component } from 'react'
 import logo from '../logo.svg';
 
 class Header extends Component {
-    inputChange(event) {
-        console.log(event.target.value);
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: 'Title',
+            keywords: ''
+        }
     }
+    inputChange(e) {
+        this.setState({ keywords: e.target.value })
+    }
+
     render() {
         return (
             <header>
                 <div
                     className="logo"
                     onClick={() => { console.log('hiya') }}>Logo</div>
-                <input onChange={this.inputChange} />
+                <input onChange={this.inputChange.bind(this)} />
+                <div> {this.state.title}</div>
+                <div> {this.state.keywords}</div>
             </header>
         )
     }
